aa723e1e339567e414b582c84722fd3561bfc3d2
Chang <nakatomicorporation1@gmail.com>
2023-08-07 13:26:11 -0700

    added multifile storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { Storage } from "aws-amplify";
 import { useState } from "react";
@@ -13,37 +13,40 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
-
 function App({ signOut }) {
-  const [fileData, setFileData] = useState();
+  const [fileData, setFileData] = useState([]);
   const [fileStatus, setFileStatus] = useState(false);
-
-  const uploadFile = async () => {
-    const file = fileData;
-
-    if (!file) {
-      alert('Please select a file before uploading');
+  const onChange = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+    setFileData(selectedFiles);
+  };
+  const uploadFiles = async () => {
+    if (fileData.length === 0) {
+      alert("Please select files before uploading");
       return;
     }
-
-    if (file.type !== 'text/csv') {
-      alert('Only CSV files are allowed');
-      return;
-    }
-
+    const allowedFileTypes = ["text/csv"];
+    const uploadPromises = fileData.map(async (file) => {
+      if (!allowedFileTypes.includes(file.type)) {
+        alert("Only CSV files are allowed");
+        return;
+      }
+      try {
+        const result = await Storage.put(file.name, file, {
+          contentType: file.type,
+        });
+        console.log("File uploaded successfully:", result);
+      } catch (error) {
+        console.error("File upload error", error);
+      }
+    });
     try {
-      const result = await Storage.put(fileData.name, fileData, {
-        contentType: fileData.type,
-      });
+      await Promise.all(uploadPromises);
       setFileStatus(true);
-      console.log(21, result);
     } catch (error) {
-      console.error('File upload error', error);
+      console.error("Error while uploading files:", error);
     }
-    
-    // console.log(21, result);
-  }
-  
+  };
   return (
     <View className="App">
       <header>
@@ -54,20 +57,23 @@ function App({ signOut }) {
         {/* <Heading level={1}>We now have Auth!</Heading> */}
       </Card>
       <div>
-        <Button id="signOut" onClick={signOut}>Sign Out</Button>
+        <Button id="signOut" onClick={signOut}>
+          Sign Out
+        </Button>
       </div>
-
       <main>
         <h2>Upload CSV files here</h2>
-        {/* <form action="/action_page.php"> */}
-          <input type="file" id="myFile" name="filename" onChange={(e) => setFileData(e.target.files[0])} />
-          {/* <input type="submit" /> */}
-          <button onClick={uploadFile}>Upload file</button>
-        {/* </form> */}
-        {fileStatus ? "File uploaded successfully" : ""}
+        <input
+          type="file"
+          hidden
+          multiple
+          accept=".csv"
+          onChange={onChange}
+        />
+        <button onClick={uploadFiles}>Upload files</button>
+        {fileStatus ? "Files uploaded successfully" : ""}
       </main>
     </View>
   );
 }
-
 export default withAuthenticator(App);
